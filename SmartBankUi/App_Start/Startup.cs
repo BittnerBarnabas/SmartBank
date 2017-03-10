50130b5d9c02956d9f624f33396523b13dd9dcd8
Bittner Barnabás <bittnerbarni@gmail.com>
2017-03-10 09:09:52 +0100

    Moved StartUp class & added logging

---
+++ SmartBankUi/App_Start/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Helpers;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Serilog;
+
+[assembly: OwinStartup(typeof(SmartBankUi.Startup))]
+namespace SmartBankUi
+{
+    public class Startup
+    {
+        private ILogger LOG = Log.ForContext<Startup>();
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            LOG.Information("Authentication settings configured");   
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Index")
+            });
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+        }
+    }
+}
